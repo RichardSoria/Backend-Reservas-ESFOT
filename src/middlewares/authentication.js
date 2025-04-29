f85663226ea10846cc56d2a93dcb7e6fc911d365
richard_soria <richard.soria@epn.edu.ec>
2025-04-29 16:34:58 -0500

    correción en los res, req y next

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,39 +1,30 @@
 import jwt from 'jsonwebtoken';
 import Admin from '../models/Admin.js';
 
-// Método para proteger rutas
-const verifyAuth = async (req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'Se debe proporcionar un token' });
+const verifyAuth = async (req, reply) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return reply.code(401).send({ message: 'Se debe proporcionar un token' });
     }
 
-    // Desconstruir el token del header
-    const { authorization } = req.headers;
-
-    // Capturar errores
     try {
-        const { id, rol } = jwt.verify(authorization.split(' ')[1], req.server.config.JWT_SECRET);
-        // Verificar el rol
+        const token = authHeader.split(' ')[1];
+        const { id, rol } = jwt.verify(token, req.server.config.JWT_SECRET);
+
         if (rol === 'administrador') {
             req.adminBDD = await Admin.findById(id).select('-password -__v');
-            next();
-        }
-        else if (rol === 'docente') {
+        } else if (rol === 'docente') {
             req.profBDD = await Admin.findById(id).select('-password -__v');
-            next();
-        }
-        else if (rol === 'estudiante') {
+        } else if (rol === 'estudiante') {
             req.estuBDD = await Admin.findById(id).select('-password -__v');
-            next();
-        }
-        else {
-            return res.status(401).send({ message: 'No tienes permisos para acceder a esta ruta' });
+        } else {
+            return reply.code(401).send({ message: 'No tienes permisos para acceder a esta ruta' });
         }
 
+        // No necesitas "next()"
     } catch (error) {
-        return res.status(401).send({ message: 'Token inválido' });
+        return reply.code(401).send({ message: 'Token inválido' });
     }
 };
 
-// Exportar el middleware
 export default verifyAuth;
